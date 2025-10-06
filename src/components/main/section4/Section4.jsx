e9eb8fe260fab4943769afb62e5aa706771808c0
Abdullohabdumalikov <a3766668@gmail.com>
2025-10-06 10:28:36 +0500

    done

--- src/components/main/section4/Section4.jsx
+++ src/components/main/section4/Section4.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./section4.css";
+import exp from "../../../assets/images/explore.png";
+
 function Section4() {
-  return <div>section4</div>;
+  return (
+    <div className="exp">
+      <div className="work">
+        <div className="acc-cerat">
+          <a href="#">Explore varieties</a>
+          <h3>Shop for your favorites meal as e dey hot.</h3>
+          <p>
+            Shop for your favorite meals or drinks and enjoy while doing it.
+          </p>
+        </div>
+        <img src={exp} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Section4;
