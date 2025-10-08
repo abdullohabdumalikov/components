66c5846a2de8970f07feee36cc016d263f4bdc7a
AzamatMurodjonov <murodjonovazamat07@gmail.com>
2025-10-08 10:36:39 +0500

    narsa

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./footer.css";
+import lo from "../../assets/images/logo.png";
+
 function Footer() {
-  return <div>footer</div>;
+  return <div>
+    <div className="sectin">
+      <div className="img">
+        <img src={lo} alt="" />
+      </div>
+      <div className="icon">
+        <i class="fa-brands fa-twitter"></i><br />
+        <i class="fa-brands fa-facebook"></i><br />
+        <i class="fa-brands fa-instagram"></i>
+      </div>
+      <div className="tex">
+        <p>Copywright 2020 Bella Onojie.com</p>
+      </div>
+    </div>
+  </div>;
 }
 
 export default Footer;
--- src/components/main/section5/Section5.jsx
+++ src/components/main/section5/Section5.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import "./section5.css";
+import rec from "../../../assets/images/Rectangle (2).png";
+
 function Section5() {
-  return <div>section5</div>;
+  return (
+    <div>
+      <section className="header">
+        <div className="img">
+          <img src={rec} alt="" />
+        </div>
+        <div className="text">
+          <h3>
+            <span>Checkout</span>
+          </h3>
+          <br />
+          <h1>When you done check out <br /> and get it delivered.</h1>
+          <br />
+          <p>When you done check out and get it <br /> delivered with ease.</p>
+        </div>
+      </section>
+      ;
+    </div>
+  );
 }
 
 export default Section5;
